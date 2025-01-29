793c678e48b62ae15569e0b2e07579e085e97aed
Nikita <nuzakov@htc-cs.ru>
2025-01-29 18:28:21 +0400

    main: switched to posgres

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,12 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
+
+console.log(process.env.DATABASE_PORT, 'port')
+console.log(process.env.DATABASE_HOST, 'host')
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+    dialect: "sqlite",
+    storage: "./database.sqlite",
   },
   production: {
-    dialect: 'postgres',
+    dialect: "postgres",
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
@@ -14,15 +17,11 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+    dialect: "postgres",
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
